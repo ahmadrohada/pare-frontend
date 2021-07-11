f0a6ccc60de722e5c95e502fb7092c356beb6e57
ahmadrohada <rohada987@gmail.com>
2021-07-12 02:17:10 +0700

    update malam

---
+++ pages/admin_skpd copy/dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row">
+    <!-- Big Chart -->
+    <div class="col-12">
+      <card>
+        ADMIN SKPD DASHBOARD
+      </card>
+    </div>
+  </div>
+</template>
+<script>
+
+import DashboardMenu from '~/components/Menu/DashboardMenu.vue';
+
+export default {
+  name: "dashoard",
+  layout: "dashboard",
+  middleware: 'auth',
+  components: {
+      DashboardMenu
+  },
+};
+</script>
+<style></style>
+

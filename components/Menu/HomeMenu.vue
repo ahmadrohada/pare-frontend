377be4a061ddebcf41166e8e61dd18210297a586
ahmadrohada <rohada987@gmail.com>
2023-07-11 15:37:00 +0700

    buku panduan

--- components/Menu/HomeMenu.vue
+++ components/Menu/HomeMenu.vue
@@ -1,16 +1,21 @@
 <template>
-  <div> 
-    <card class="card-user" style="margin-top:-10px !important;height: 90px !important; background: transparent !important;">
+  <div>
+    <card class="card-user"
+      style="margin-top:-10px !important;height: 90px !important; background: transparent !important;">
       <div class="author">
-         <img 
-          src="~/static/img/bg_profile.png"
-          alt="..."
-          class="bg_photo"
-        />
+        <img src="~/static/img/bg_profile.png" alt="..." class="bg_photo" />
       </div>
     </card>
-   
-   
+    <el-menu :default-openeds="[]" background-color="transparent" text-color="#fff" active-text-color="#ffd04b"
+      :router="true" :default-active="activeLink">
+
+      <el-menu-item @click="bukuPanduan()">
+
+        <i class="el-icon-notebook-1"></i>
+        <span>Buku Panduan</span>
+      </el-menu-item>
+    </el-menu>
+
 
 
     <!-- <sidebar-item
@@ -41,50 +46,55 @@
       
     >
     </sidebar-item> -->
-   
+
   </div>
 </template>
 
 <script>
 export default {
   name: "HomeMenu",
-  data(){
-    return{
+  data() {
+    return {
       isAdminSkpd: false,
-      isPersonal : false,
-      isAdminBkpsdm : false,
-      roles:"",
+      isPersonal: false,
+      isAdminBkpsdm: false,
+      roles: "",
 
-    } 
+    }
   },
   methods: {
     functionAdminSkpd(roles) {
-     for(var i=0; i < roles.length; i++){
-        if( roles[i].role_id == 2){
+      for (var i = 0; i < roles.length; i++) {
+        if (roles[i].role_id == 2) {
           return true
         }
       }
       return false
     },
     functionPersonal(roles) {
-     for(var i=0; i < roles.length; i++){
-        if( roles[i].role_id == 1){
+      for (var i = 0; i < roles.length; i++) {
+        if (roles[i].role_id == 1) {
           return true
         }
       }
       return false
     },
     functionAdminBkpsdm(roles) {
-     for(var i=0; i < roles.length; i++){
-        if( roles[i].role_id == 3){
+      for (var i = 0; i < roles.length; i++) {
+        if (roles[i].role_id == 3) {
           return true
         }
       }
       return false
-    }
+    },
+    bukuPanduan() {
+        window.open('https://api-pare-v3.bkpsdm.karawangkab.go.id/buku-panduan', '_blank', 'noreferrer');
+        //http://localhost:8000
+        //https://api-pare-v3.bkpsdm.karawangkab.go.id
+      },
   },
   mounted() {
-    
+
     var roles = this.$auth.$state.user.roles
     this.isPersonal = this.functionPersonal(roles)
     this.isAdminSkpd = this.functionAdminSkpd(roles)
@@ -95,4 +105,19 @@ export default {
 </script>
 
 <style lang="scss">
+.el-menu {
+  border-right: none !important;
+}
+
+.el-submenu__title:hover,
+.el-menu-item:hover {
+  background-color: rgba(5, 117, 93, 0.288) !important;
+}
+
+.el-menu-item,
+.el-submenu__title {
+  height: 40px !important;
+  line-height: 40px !important;
+  background-color: transparent !important;
+}
 </style>
